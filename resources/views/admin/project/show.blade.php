a8ea35be579229928a5d43005ab8c07b5e5be96c
AndreaGallini <andrea.gallini01@libero.it>
2023-01-12 09:20:50 +0100

    fix img show

--- resources/views/admin/project/show.blade.php
+++ resources/views/admin/project/show.blade.php
@@ -13,7 +13,7 @@
                     <p> Collaboratori : {{ $project->collaboratori }}</p>
                 @endif
                 <p>{{ $project->descrizione }}</p>
-                <img class="mb-3
+                <img class="mb-3 show_img
                 " src="{{ asset('storage/' . $project->img) }}">
                 <button class="btn btn-primary mb-4"><a class="button_a"
                         href="https://github.com/AndreaGallini/{{ $project->slug }}" target="_blank">vai al
