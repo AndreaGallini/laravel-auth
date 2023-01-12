7591f365f97eea1bd19bc8b3fb30fd8d8f4e9ebc
AndreGallini <andrea.gallini01@libero.it>
2023-01-12 13:57:24 +0100

    relations done

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -4,13 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Support\Str;
 
 class Project extends Model
 {
     use HasFactory;
-    protected $fillable = ['nome_progetto', 'slug','descrizione', 'collaboratori', 'autore', 'data_inizio_progetto','img'];
+    protected $guarded = [];
     public static function createSlug($nome_progetto){
         return Str::slug($nome_progetto, '-');
+    }
+    public function category(): BelongsTo
+    {
+        $this->belongsTo(Category::class);
     }
 }
